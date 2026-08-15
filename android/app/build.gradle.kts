2a43af25d4f15a99e2dc84a02287e820bfc82393
Marvel Lawson <lawsonmarvel@gmail.com>
2026-08-15 13:39:16 +0200

    fix: Android CI memory/lint/NDK + iOS deployment target 15.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,6 +54,10 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    lint {
+        checkReleaseBuilds = false
+    }
 }
 
 flutter {
